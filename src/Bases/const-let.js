882f03761b71d2032b3887fd142461960ecf1841
unknown <angela230293@gmail.com>
2021-09-15 15:51:18 -0500

    1ra clase de taller

---
+++ src/Bases/const-let.js
@@ -0,0 +1,25 @@
+console.log('Bienvenido al taller de React');
+
+//variables y constantes
+
+//const nombre = 'Angela';
+//const apellido = 'Calderon';
+
+var edad=0;
+//let edad2 = 0;
+const anioNacimiento = 1993;
+const anioActual = 2021
+
+if(anioActual === 2021){
+
+    let edad = 0;
+
+    edad =anioActual - anioNacimiento;
+    //edad2 =anioActual - anioNacimiento;
+
+    console.log('Edad actual entorno del if', edad);
+
+}
+
+console.log('Edad actual', edad);
+//console.log('Edad 2 actual', edad2);
